a1239be1cd82acf1cd71bba02565671b1116a50d
sup95 <sup0508@gmail.com>
2019-11-28 23:35:38 +0530

    Completed exercises - Introduction

--- Introduction/Object expressions/src/Task.kt
+++ Introduction/Object expressions/src/Task.kt
@@ -2,6 +2,6 @@ import java.util.*
 
 fun getList(): List<Int> {
     val arrayList = arrayListOf(1, 5, 2)
-    Collections.sort(arrayList, object {})
+    Collections.sort(arrayList) { p0, p1 -> p1?.compareTo(p0) ?: 0 }
     return arrayList
 }
