df48a3c74181a19a1c8935349811f9356cda695b
sup95 <sup0508@gmail.com>
2019-12-02 15:39:02 +0530

    complete exercises - Collections

--- Collections/Partition/src/Task.kt
+++ Collections/Partition/src/Task.kt
@@ -1,2 +1,8 @@
 // Return customers who have more undelivered orders than delivered
-fun Shop.getCustomersWithMoreUndeliveredOrdersThanDelivered(): Set<Customer> = TODO()
+fun Shop.getCustomersWithMoreUndeliveredOrdersThanDelivered(): Set<Customer> =
+        customers.filter { checkOrderCount(it.orders) }.toSet()
+
+fun checkOrderCount(order: List<Order>) : Boolean{
+    val (delivered, undelivered) = order.partition { it.isDelivered }
+    return undelivered.size > delivered.size
+}
