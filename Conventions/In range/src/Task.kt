e60247925faf011393e7131b3fe38b8d852a0c8b
sup95 <sup0508@gmail.com>
2019-12-02 11:44:16 +0530

    complete exercises - Conventions

--- Conventions/In range/src/Task.kt
+++ Conventions/In range/src/Task.kt
@@ -1,4 +1,6 @@
-class DateRange(val start: MyDate, val endInclusive: MyDate)/* TODO */
+class DateRange(val start: MyDate, val endInclusive: MyDate) {
+    operator fun contains(d: MyDate): Boolean = d in start..endInclusive
+}
 
 fun checkInRange(date: MyDate, first: MyDate, last: MyDate): Boolean {
     return date in DateRange(first, last)
